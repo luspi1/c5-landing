34e81de4ca67d095b79361f9abe3c0089790b037
luspi1 <47425618+luspi1@users.noreply.github.com>
2023-10-16 17:30:07 +0300

    правки путей

--- gulpfile.js
+++ gulpfile.js
@@ -340,7 +340,7 @@ const htmlMinify = () => {
     .pipe(dest(buildFolder))
 }
 
-const zipFiles = () => {
+const zipFiles = (done) => {
   del.sync([`${buildFolder}/*.zip`])
   return src(`${buildFolder}/**/*.*`, {})
     .pipe(
