fe11c42baee2157e2dbcd9c553df366021674a4f
ludaalt <luda.tamb@mail.ru>
2023-11-20 14:43:21 +0300

    корректировка загрузки файлов

--- src/js/components/fileUpload.js
+++ src/js/components/fileUpload.js
@@ -20,9 +20,46 @@ export const initFileUploading = () => {
       const uploadName = fileUploadEl.querySelector('.file-upload__name')
       const uploadDate = fileUploadEl.querySelector('.file-upload__date')
       const uploadTime = fileUploadEl.querySelector('.file-upload__time')
-      const deleteButton = fileUploadEl.querySelector(
-        '.file-upload__delete-btn',
-      )
+
+      //////
+
+      const exampleInitInputFile = document
+        .querySelector('#edit-document-modal')
+        .querySelector("[id='edit-file[2]']")
+
+      if (exampleInitInputFile) {
+        const myFile = new File(['Hello World!'], 'myFile.pdf', {
+          type: 'text/plain',
+          lastModified: new Date(),
+        })
+
+        const dataTransfer = new DataTransfer()
+        dataTransfer.items.add(myFile)
+        exampleInitInputFile.files = dataTransfer.files
+
+        const fileUpload = exampleInitInputFile.closest('.file-upload')
+
+        //exampleInitInputFile.classList.add('_loaded')
+
+        const info = fileUpload.querySelector('.file-upload__info')
+        info.style.display = 'block'
+
+        const uploadDate = fileUpload.querySelector('.file-upload__date')
+        const uploadTime = fileUpload.querySelector('.file-upload__time')
+
+        if (uploadDate && uploadTime) {
+          uploadDate.textContent = getCurrentDate()
+          uploadTime.textContent = getCurrentTime()
+        }
+
+        fileUpload
+          .querySelector('.file-upload__delete-btn')
+          .addEventListener('click', () => {
+            info.style.display = 'none'
+          })
+      }
+
+      //////
 
       uploadInput.addEventListener('input', (evt) => {
         let targetInput = evt.currentTarget
@@ -30,13 +67,24 @@ export const initFileUploading = () => {
         let reader = new FileReader()
         reader.readAsDataURL(fileItem)
 
-        reader.addEventListener('load', (e) => {
+        reader.addEventListener('load', () => {
+          //////
+          if (
+            fileUploadEl.querySelector('.file-upload__info').style.display ===
+            'none'
+          ) {
+            fileUploadEl.querySelector('.file-upload__info').style.display =
+              'block'
+          }
+          //////
+
           if (uploadName) {
             uploadName.textContent = fileItem.name ?? ''
             uploadName.href = reader.result
           }
 
           if (uploadDate) {
+            console.log(uploadDate)
             uploadDate.textContent = getCurrentDate()
           }
 
@@ -44,13 +92,6 @@ export const initFileUploading = () => {
             uploadTime.textContent = getCurrentTime()
           }
 
-          if (deleteButton) {
-            deleteButton.textContent = 'удалить файл'
-          }
-          // if (uploadSize) {
-          //   uploadSize.textContent =
-          //     `${Math.round(fileItem.size / 1000)} КБ` ?? ''
-          // }
           fileUploadEl.classList.add('_loaded')
         })
 
